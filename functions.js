75c414941353d8821d444fe4901f3ae73593e420
urzp <ermak80_pass@mail.ru>
2023-09-12 15:41:28 +0300

    add function wait ready for any element on tilda page

--- functions.js
+++ functions.js
@@ -8,6 +8,31 @@ function eventLoadElement( elementQuerySelector, callBack, interval = 300){
     }, interval)
 }
 
+//wait ready for any element on tilda page
+
+async function WaitReadyElement(elementQuerySelector, timeRequest = 300, limrequest = 10){
+		
+    let promise = new Promise((resolve, reject)=>{
+        let counter = 0
+        let tryGetElement = setInterval(()=>{
+            counter++ ; //console.log('tryGet ' + counter)
+            if(counter > limrequest) { clearInterval(tryGetElement); return reject(new Error("can't find element " + elementQuerySelector))}
+            if(!document.querySelector(elementQuerySelector)) return false
+            clearInterval(tryGetElement); return resolve()
+        }, timeRequest)
+    })
+
+    await promise		
+}
+
+//use exsamle
+// async function init(){
+//     await WaitReadyElement('.counter .t-form')
+//     document.querySelector('.counter .t-form').setAttribute('id','vue_app');
+// }
+
+// init()
+
 //get Product of tilda
 
 function productParseTilda() {
